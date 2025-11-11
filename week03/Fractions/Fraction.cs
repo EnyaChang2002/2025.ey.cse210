2cf19acd362ba8848499610105fe503ee7b92fd1
EnyaChang2002 <yyy2011taiwan@gmail.com>
2025-11-11 18:33:35 +0800

    practive

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,49 @@
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public void GetTop()
+    {
+        Console.WriteLine(_top);
+    }
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+    public void GetBottom()
+    {
+        Console.WriteLine(_bottom);
+    }
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        string _fString = $"{_top}" + "/" + $"{_bottom}";
+        return _fString;
+    }
+    public double GetDecimalValue()
+    {
+        double _fDouble = (double)_top / (double)_bottom;
+        return _fDouble;
+    }
+}
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction _newF = new Fraction();
+        _newF.SetTop(1);
+        Console.WriteLine(_newF.GetFractionString());
+        Console.WriteLine(_newF.GetDecimalValue());
+
+        _newF.SetTop(5);
+        Console.WriteLine(_newF.GetFractionString());
+        Console.WriteLine(_newF.GetDecimalValue());
+
+        _newF.SetTop(3);
+        _newF.SetBottom(4);
+        Console.WriteLine(_newF.GetFractionString());
+        Console.WriteLine(_newF.GetDecimalValue());
+
+        _newF.SetTop(1);
+        _newF.SetBottom(3);
+        Console.WriteLine(_newF.GetFractionString());
+        Console.WriteLine(_newF.GetDecimalValue());
     }
 }
